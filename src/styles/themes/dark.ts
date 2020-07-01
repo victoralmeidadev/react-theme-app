bd114464bb0d31ee37d1f04af89afeadf3950629
C1311569 Victor Hugo Rodrigues Pereira Mendonca De Almeida <c1311569@interno.bb.com.br>
2020-06-30 21:45:44 -0300

    feature: Aplicação de estudo com temas light e dark

---
+++ src/styles/themes/dark.ts
@@ -0,0 +1,11 @@
+export default {
+    title: 'dark',
+
+    colors: {
+        primary: '#470063',
+        secondary: '#B30089',
+
+        background: '#333',
+        text: '#f5f5f5'
+    },
+};
